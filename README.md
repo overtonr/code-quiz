4906235b4c5ac43e71bb4b1bf392edb02714280f
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-03 22:31:43 -0700

    README completed (just needs code snippet, screenshots, and deployed link

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# challenge-code-quiz
+# Challenge : Code Quiz
+-
+## Description:
+This quiz was built with no starter code given. It is a 10 question multiple-choice quiz that lasts 60 seconds. Users also have the option to store their high-scores when the quiz is over, and a button with the ability to clear their stored high scores.
+
+## Usage: 
+This timed multiple-choice quiz starts when the start button is pressed. Users have 60 seconds to answer all 10 questions. The faster the users selects all the correct answers, the higher their score will be. If a user chooses incorrectly, 10 seconds will be penalized from the time-score. The goal is to try to get the most amount of questions correct in the fastest time possible.
+When the game is over, users have the ability to store their score alongside their initials. From this page, they have the ability to start the quiz and attept to get a faster time. There is also the option to clear these scores from the leaderboard.
+
+## Technologies Used:
+* Github
+* HTML
+* CSS
+* JavaScript
+* VS Code
+
+## License 
+Please refer to the license in the repository.
+
--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@
 //an incorrect guess subtracts 5 seconds from the clock
 //game ends when either the time reaches 0 OR all questions are answered
 //when game is over, you can save initials and score
-var gameBody = document.querySelector("body");
+// var gameBody = document.querySelector("body");
 var startPage = document.querySelector("#start-page");
 var startButton = document.querySelector(".start-game");
 var gameBox = document.querySelector("#game-box");
@@ -15,4 +15,32 @@ var initialsInput = document.querySelector("#initials-input");
 var submitButton = document.querySelector("#submit-button");
 var rankPage = document.querySelector("#rank-page");
 var scoreRank = document.querySelector("#score-rank");
+var clearScores = document.querySelector("#clear-scores");
+
+var gameOver = true;
+var timeLeft = 60;
+var timeInterval;
+//var localSavedScore = localStorage.getItem("initials","timeLeft")
+
+startButton.addEventListener("click", game);
+
+function resetGame(){
+    gameOver = false;
+    timeLeft = 60;
+//something to do with the array of questions
+}
+
+function countdown(){
+    if(!gameOver) {
+        if (timeLeft > 0) {
+            countdownBox.textContent = "time left: " + timeLeft;
+//decrements time when time > 0
+            timeLeft--;
+        } else {
+            gameOver = true;
+//something to navigate to highsores page
+        }
+    }
+}
+
 
