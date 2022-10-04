5f2aea8d7e491dfd33be731dc4e3013f97a7f56d
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-03 19:01:49 -0700

    Starter code and basic outline/pseudocode

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//Start button: starts the quiz: starts the timer, displays question
+//when a question is answered, a new question is presented
+//an incorrect guess subtracts 5 seconds from the clock
+//game ends when either the time reaches 0 OR all questions are answered
+//when game is over, you can save initials and score
+
+
+
+
+
+
+console.log(Math.random())
