0b9e08f465500ba7adb92c2702e7393211c0709a
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-03 23:25:43 -0700

    Button added to start timer

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,4 @@
-//Start button: starts the quiz: starts the timer, displays question
-//when a question is answered, a new question is presented
-//an incorrect guess subtracts 5 seconds from the clock
-//game ends when either the time reaches 0 OR all questions are answered
-//when game is over, you can save initials and score
-// var gameBody = document.querySelector("body");
+
 var startPage = document.querySelector("#start-page");
 var startButton = document.querySelector(".start-game");
 var gameBox = document.querySelector("#game-box");
@@ -18,16 +13,41 @@ var scoreRank = document.querySelector("#score-rank");
 var clearScores = document.querySelector("#clear-scores");
 
 var gameOver = true;
-var timeLeft = 60;
+var timeLeft = 10;
 var timeInterval;
+
+//when a question is answered, a new question is presented
+//an incorrect guess subtracts 5 seconds from the clock
+//game ends when either the time reaches 0 OR all questions are answered
+//when game is over, you can save initials and score
+// var gameBody = document.querySelector("body");
+
+
+startButton.addEventListener("click", startGame());
+
+//Start button: starts the quiz: starts the timer, displays question
+    function startGame(){
+        var timeInterval = setInterval (function() {
+            timeLeft--;
+            countdownBox.textContent = timeLeft;
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            gameFinished();
+        }
+        },1000);
+    }
+
+
+
 //var localSavedScore = localStorage.getItem("initials","timeLeft")
 
-startButton.addEventListener("click", game);
 
 function resetGame(){
     gameOver = false;
     timeLeft = 60;
 //something to do with the array of questions
+    clearInterval(timeInterval);
+
 }
 
 function countdown(){
@@ -41,6 +61,15 @@ function countdown(){
 //something to navigate to highsores page
         }
     }
+}
+
+
+function setTime(){
+    var timeInterval = setInterval(function(){
+        secondsLeft--;
+        countdownBox.textContent = timeLeft 
+    }
+    )
 }
 
 
